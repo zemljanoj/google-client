9d94abcd2ea0dc18d580b84c3cc4b090ab476e46
Andrey Inyagin <zemljanoj.i@gmail.com>
2018-05-31 09:23:22 +0300

    Add factory and service.

---
+++ Model/ClientFactory.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Copyright (c) 2018.
+ * @author Andrey Inyagin <zemljanoj.i@gmail.com>
+ */
+namespace Zemljanoj\GoogleClient\Model;
+/**
+ * Class \Zemljanoj\GoogleClient\Model\ClientFactory
+ */
+class ClientFactory
+{
+
+}
---
+++ Service/GetClient.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Copyright (c) 2018.
+ * @author Andrey Inyagin <zemljanoj.i@gmail.com>
+ */
+namespace Zemljanoj\GoogleClient\Service;
+/**
+ * Class \Zemljanoj\GoogleClient\Service\GetClient
+ */
+class GetClient
+{
+    public function execute()
+    {
+
+    }
+}

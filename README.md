f51d961e490ab8167941b2cd6c19e1c5185f0f0b
Andrey Inyagin <zemljanoj.i@gmail.com>
2018-05-31 08:31:06 +0300

    Added initial files for magento module.

---
+++ README.md
@@ -0,0 +1,1 @@
+Zemljanoj_GoogleClient module.
---
+++ registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Copyright (c) 2018.
+ * @author Andrey Inyagin <zemljanoj.i@gmail.com>
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Zemljanoj_GoogleClient',
+    __DIR__
+);

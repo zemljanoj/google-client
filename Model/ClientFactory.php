a72416bbcb412e70b6609bd6ff3b62ba93f23809
Andrey Inyagin <zemljanoj.i@gmail.com>
2018-05-31 10:12:12 +0300

    Configure the Get Client service.

--- Model/ClientFactory.php
+++ Model/ClientFactory.php
@@ -9,5 +9,57 @@ namespace Zemljanoj\GoogleClient\Model;
  */
 class ClientFactory
 {
+    const CLASS_NAME = 'Google_Client';
+    /**
+     * Object Manager
+     *
+     * @var \Magento\Framework\ObjectManagerInterface
+     */
+    protected $_objectManager;
 
+    /**
+     * Client config.
+     * @var array
+     */
+    private $config;
+
+    /**
+     * Client action.
+     * @var array
+     * [<methodName1> => [<argument1>, ...], ...]
+     */
+    private $actions;
+
+    /**
+     * Construct
+     *
+     * @param \Magento\Framework\ObjectManagerInterface $objectManager
+     */
+    public function __construct(
+        \Magento\Framework\ObjectManagerInterface $objectManager,
+        array $config,
+        array $actions
+    ) {
+        $this->_objectManager = $objectManager;
+        $this->config = $config;
+        $this->actions = $actions;
+    }
+
+    /**
+     * Create model
+     *
+     * @param array $config
+     * @return \Google_Client
+     */
+    public function create(array $config = [], array $actions = [])
+    {
+        $config = array_merge($this->config, $config);
+        $actions = array_merge($this->actions, $actions);
+        $model = $this->_objectManager->create(self::CLASS_NAME, $config);
+        foreach ($actions as $method => $arguments) {
+            $model->{$method}(array_values($arguments));
+        }
+
+        return $model;
+    }
 }
--- Service/GetClient.php
+++ Service/GetClient.php
@@ -5,12 +5,67 @@
  */
 namespace Zemljanoj\GoogleClient\Service;
 /**
+ * Create and authorization the google client object.
  * Class \Zemljanoj\GoogleClient\Service\GetClient
  */
 class GetClient
 {
+    const CONFIG_FILE_NAME = 'client_id.json';
+
+    const CONFIG_FILE_PATH = 'etc';
+
+    /**
+     * Client factory.
+     * @var \Zemljanoj\GoogleClient\Model\ClientFactory
+     */
+    private $clientFactory;
+
+    /**
+     * @var \Magento\Framework\Filesystem\Io\File
+     */
+    private $ioFile;
+
+    /**
+     * GetClient constructor.
+     *
+     * @param \Magento\Framework\Filesystem\Io\File $ioFile
+     * @param \Zemljanoj\GoogleClient\Model\ClientFactory $clientFactory
+     */
+    public function __construct (
+        \Magento\Framework\Filesystem\Io\File $ioFile,
+        \Zemljanoj\GoogleClient\Model\ClientFactory $clientFactory
+    ) {
+        $this->clientFactory = $clientFactory;
+        $this->ioFile = $ioFile;
+    }
+
+    /**
+     * Execute.
+     * @return \Google_Client
+     */
     public function execute()
     {
+        $client = $this->clientFactory->create();
+        $config = $this->ioFile->read($this->getFileName());
+        $accessToken = json_decode($config, true);
+        $client->setAccessToken($accessToken);
+        if ($client->isAccessTokenExpired()) {
+            $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+            $config = json_encode($client->getAccessToken());
+            $this->ioFile->write($this->getFileName(), $config);
+        }
 
+        return $client;
+    }
+
+    /**
+     * Get config file name.
+     * @return string
+     */
+    private function getFileName()
+    {
+        return dirname(dirname(__file__))
+               . '/' . self::CONFIG_FILE_PATH
+               . '/' . self::CONFIG_FILE_NAME;
     }
 }
